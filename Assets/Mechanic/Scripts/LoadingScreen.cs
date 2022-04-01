29fcf4c22dcfbc143dac5e2f08b037d1fc6276f8
FrozenPanda <bahadirbilgili@gmail.com>
2022-04-01 23:42:42 +0300

    SmoothSceneSwap

--- Assets/Mechanic/Scripts/LoadingScreen.cs
+++ Assets/Mechanic/Scripts/LoadingScreen.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,76 @@ using UnityEngine.UI;
 public class LoadingScreen : MonoBehaviour
 {
     public Text LoadingText;
+    public static LoadingScreen instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private enum Loading
+    {
+        Load1,
+        Load2,
+        Load3
+    }
+
+    private Loading _loading;
+    
     void Start()
     {
-        StartCoroutine(waitAndClose());
+        gameObject.SetActive(false);
+        LoadingText.text = "Loading.  ";
+        //StartCoroutine(waitAndClose());
+    }
+
+    private float timer = 0.2f;
+    private void Update()
+    {
+        switch (_loading)
+        {
+            case Loading.Load1:
+
+                if (timer > 0f)
+                {
+                    timer -= Time.deltaTime;
+                }
+                else
+                {
+                    _loading = Loading.Load2;
+                    timer = 0.2f;
+                }
+                
+                break;
+            case Loading.Load2:
+                
+                if (timer > 0f)
+                {
+                    timer -= Time.deltaTime;
+                }
+                else
+                {
+                    _loading = Loading.Load3;
+                    timer = 0.2f;
+                    LoadingText.text = "Loading.. ";
+                }
+                
+                break;
+            case Loading.Load3:
+                
+                if (timer > 0f)
+                {
+                    timer -= Time.deltaTime;
+                }
+                else
+                {
+                    _loading = Loading.Load1;
+                    timer = 0.2f;
+                    LoadingText.text = "Loading...";
+                }
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 
     IEnumerator waitAndClose()
--- Assets/Mechanic/Scripts/SceneLoad.cs
+++ Assets/Mechanic/Scripts/SceneLoad.cs
@@ -6,19 +6,50 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoad : MonoBehaviour
 {
+    private bool SceneLoadAlready = false;
     private void OnTriggerEnter(Collider other)
     {
+        if (SceneLoadAlready)
+        {
+            return;
+        }
+        
         if (other.tag == "Scene")
         {
-            NextScene();    
+            Debug.Log("OtherScene" + other.gameObject.name);
+            Destroy(other.gameObject);
+            NextScene();
+            SceneLoadAlready = true;
         }
     }
 
     private void NextScene()
     {
+        
         int x = SceneManager.GetActiveScene().buildIndex;
         x++;
         x %= SceneManager.sceneCountInBuildSettings;
-        SceneManager.LoadScene(x);
+        //SceneManager.LoadScene(x);
+        StartCoroutine(LoadYourAsyncScene(x));
+        LoadingScreen.instance.gameObject.SetActive(true);
+    }
+    
+    IEnumerator LoadYourAsyncScene(int sceneIndex)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+        
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        
     }
 }
